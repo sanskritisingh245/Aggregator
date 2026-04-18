39236f9f03ffbd8444bb4837b84eac0942f8bf0e
sanskritisingh245 <sanskritisinghh2005@gmail.com>
2026-04-18 20:02:40 +0530

    geckodata merged

--- src/index.ts
+++ src/index.ts
@@ -44,31 +44,69 @@ cron.schedule('*/30 * * * * *',async()=>{
             fetchDataWithRetry('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd or https://api.coingecko.com/api/v3/coins/solana')
         ]);
 
-        const validresponse=[];
-
+        let storage:  Record<string, Token> ={};
+        let normaliseDexData:Token[]=[];
+        let normaliseJupiterData:Token[] =[];
+        let normaliseGeckoData:Token[]=[];
 
         if(dex.status === "fulfilled" && dex.value != null ){
             const dexData=dex.value;
-            normaliseDex(dexData)
+            normaliseDexData=normaliseDex(dexData);
         }
 
         if(jupiter.status=== "fulfilled" && jupiter.value != null ){
             const jupiterData=jupiter.value;
-            normaliseJupiter(jupiterData)
+            normaliseJupiterData=normaliseJupiter(jupiterData);
         }
 
         if(gecko.status ==="fulfilled" && gecko.value != null){
             const geckoData=gecko.value;
-            normaliseGecko(geckoData)
+            normaliseGeckoData=normaliseGecko(geckoData);
         }
+        
+        normaliseDexData.forEach((token)=>{
+            storage[token.address]=token;
+        })
 
+        normaliseJupiterData.forEach((token)=>{
+            if(storage[token.address]){
+                const exsisting=storage[token.address]
+                if(exsisting){
+                    if(exsisting.volume24h=== null && token.volume24h != null){
+                        exsisting.volume24h=token.volume24h
+                    }
+                }
+            }else {
+                storage[token.address]=token;
+            }
+        })
+
+        const finalToken=Object.values(storage)
+        finalToken.forEach((token)=>{
+            const geckoMatch=normaliseGeckoData.find(geckoToken =>
+                geckoToken.symbol.toLowerCase() ===token.symbol.toLowerCase() && 
+                geckoToken.name.toLowerCase() === token.name.toLowerCase()
+            )
+            if(geckoMatch){
+                if(token.marketCap === null){
+                    token.marketCap=geckoMatch.marketCap
+                }
+                if(token.priceChange24h === null){
+                    token.priceChange24h = geckoMatch.priceChange24h
+                }
+                if(token.volume24h === null){
+                    token.volume24h = geckoMatch.volume24h
+                }
+            }
+            
+        })
 
     }catch(err:any){
         console.log(err.message)
     }
 })
 
-function normaliseDex(dexData):Token[]{
+function normaliseDex(dexData: { pairs: any[] }):Token[]{
     //let tokens=[];
     return dexData.pairs.map((pair)=>{
         return {
@@ -85,7 +123,7 @@ function normaliseDex(dexData):Token[]{
     }) 
 }
 
-function normaliseJupiter(jupiterData):Token[]{
+function normaliseJupiter(jupiterData:any[]):Token[]{
     return jupiterData.map((token)=>{
         return {
             address:token.address,
@@ -102,7 +140,7 @@ function normaliseJupiter(jupiterData):Token[]{
     })
 }
 
-function normaliseGecko(geckoData):Token[]{
+function normaliseGecko(geckoData:any[]):Token[]{
     return geckoData.map((coin)=>{
         return {
             address:coin.id,
@@ -114,8 +152,6 @@ function normaliseGecko(geckoData):Token[]{
             priceChange24h:coin.price_change_percentage_24h,
             liquidity:null,
             source:"coingecko" 
-
         }
-    })
-    
+    })  
 }
